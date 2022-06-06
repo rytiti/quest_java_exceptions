0ce2855e36f2e0279b942a67838080aecf3ca84f
Thibault Bardinet-Langlois <langlois.bardinet.thibault@gmail.com>
2022-06-06 22:39:19 +0200

    set try catch block

--- src/main/OddSum.java
+++ src/main/OddSum.java
@@ -17,7 +17,12 @@ public class OddSum {
             System.out.println();
         }
         for (int i = 0; i < args.length; i++) {
-            listInt.add(Integer.parseInt(args[i]));
+        	try {
+        		listInt.add(Integer.parseInt(args[i]));        		
+        	} catch (NumberFormatException e) {
+        		System.out.println("'" + args[i] + "' n'est pas un nombre!");
+        		return -1;
+        	}
         }
 
         res = sum(listInt);
